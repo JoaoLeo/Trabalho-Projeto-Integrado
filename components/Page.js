cb612d72a582b384800c0854d8ff2386b85f1f9c
Miguel dos Santos <migueldossantos0096@gmail.com>
2023-04-16 14:41:48 -0300

    inciando componentes

---
+++ components/Page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import { Container } from 'react-bootstrap'
+
+const Page = () => {
+  return (
+    <>
+    <Header/>
+    <Container>
+    Page
+    </Container>
+    <Footer/>
+    </>
+  )
+}
+
+export default Page
