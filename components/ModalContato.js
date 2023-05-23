9df7bdcb08227645644ee94accce63832cd453b5
Pedro Luiz R. de Souza <pedro.r.souza212@gmai.com>
2023-05-23 10:05:17 -0300

    Alterando ModalContato

--- components/ModalContato.js
+++ components/ModalContato.js
@@ -1,58 +1,62 @@
 import React, { useState } from 'react'
-import { Modal } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
+import { IoMdContact } from 'react-icons/io';
+
 
 const ModalContato = () => {
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    let [contatos, setContatos] = useState({
-        github: "",
-        email: "",
-        telefone: ""
-         })
-    return (
-      <>
-      <div className='text-black'>
-        <span variant="primary" onClick={()=>{
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  let [contatos, setContatos] = useState({
+    github: "",
+    email: "",
+    telefone: ""
+  })
+  return (
+    <>
+      <div>
+        <span onClick={() => {
           contatos.github = <a href="https://github.com/JoaoLeo" target="_blank"> JoaoLeo </a>
           contatos.email = "joaoleonardo9921@gmail.com"
           contatos.telefone = "(61)99889178"
           setContatos(contatos)
           handleShow()
         }}>
-        &bull; João Leonardo
+          <Button variant="success" ><IoMdContact/> João Leonardo</Button>
         </span>
-        <span variant="primary" onClick={() =>{
-           contatos.github = <a href="https://github.com/wolacii" target="_blank"> wolacii</a>
-           contatos.email = "Jonathan.Oliver767@gmail.com"
-           contatos.telefone = "(61)986447252"
-           setContatos(contatos)
-           handleShow()
+        <span onClick={() => {
+          contatos.github = <a href="https://github.com/wolacii" target="_blank"> wolacii</a>
+          contatos.email = "Jonathan.Oliver767@gmail.com"
+          contatos.telefone = "(61)986447252"
+          setContatos(contatos)
+          handleShow()
         }}>
-        &bull;Jonathan Oliveira
+          <Button variant="success" ><IoMdContact/> Jonathan Oliveira</Button>
         </span>
-        <span variant="primary" onClick={() =>{
-           contatos.github = <a href="https://github.com/M1guelSantos" target="_blank"> M1guelSantos</a>
-           contatos.email = "migueldossantos096@gmail.com"
-           contatos.telefone = "(61)981745308"
-           setContatos(contatos)
-           handleShow()
+        <span onClick={() => {
+          contatos.github = <a href="https://github.com/M1guelSantos" target="_blank"> M1guelSantos</a>
+          contatos.email = "migueldossantos096@gmail.com"
+          contatos.telefone = "(61)981745308"
+          setContatos(contatos)
+          handleShow()
         }}>
-        &bull; Miguel dos Santos
+          <Button variant="success" ><IoMdContact/> Miguel dos Santos</Button>
         </span>
-        <span variant="primary" onClick={()=>{
+        <span onClick={() => {
           contatos.github = <a href="https://github.com/pedroooluiz" target="_blank">Pedro Luiz</a>
           contatos.email = "pedroluizrodrigues337@gmail.com"
           contatos.telefone = "(61)998505826"
           setContatos(contatos)
           handleShow()
         }}>
-         &bull;Pedro Luiz
+          <Button variant="success" ><IoMdContact/> Pedro Luiz</Button>
         </span>
-  
+
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>Contatos</Modal.Title>
+            <Modal.Title>
+              Contatos
+            </Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <h5> Github: {contatos.github} </h5>
@@ -60,9 +64,9 @@ const ModalContato = () => {
             <h5> Telefone: {contatos.telefone} </h5>
           </Modal.Body>
         </Modal>
-        </div>
-      </>
-    );
-  }
-  
-  export default ModalContato;
+      </div>
+    </>
+  );
+}
+
+export default ModalContato;
