979ac2b8be741da58e8e9efe803d36c243db927f
Miguel dos Santos <migueldossantos0096@gmail.com>
2023-06-04 16:22:18 -0300

    adicionando lib para o carrosel

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,55 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
-import { Button, Card, Carousel, Col, Container, Row, } from 'react-bootstrap';
+import { Button, Card, Col, Container, Row, } from 'react-bootstrap';
 import apiDeputados from '@/services/apiDeputados';
+import styles from '@/styles/carrosel.module.css';
+import { motion } from 'framer-motion';
+import {useState, useEffect, useRef} from 'react'
+import { set } from 'react-hook-form';
 
 export default function Home({ deputados }) {
+
+  const carousel = useRef();
+  const [width, setWidth] = useState(0)
+
+  useEffect(() => {
+     console.log(carousel.current?.scrollWidth, carousel.current?.offsetWidth)
+     setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth)
+  }, [])
+  
   return (
     <>
       <Header />
       <Container>
-        <Row className="justify-content-md-center">
+        <Row className={styles.carrosel}>
           <Col></Col>
-          <Col md={9}>
-            <Carousel variant='dark'>
-              
-
-
-              {deputados.map(item => (
-                <Carousel.Item >
-
-
-                  <Card style={{ width: '18rem' }} md='auto'>
-                    <Card.Img variant="top" src={item.urlFoto} />
-                    <Card.Body>
-                      <Card.Title>Card Title</Card.Title>
-                      <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                      </Card.Text>
-                      <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                  </Card>
-
-                </Carousel.Item>
-              ))}
-
-
-
-            </Carousel>
+          <Col>
+            <motion.div ref={carousel} className={styles.carousel} whileTap={{cursor: "grabbing"}}>
+              <motion.div 
+              className={styles.inner}
+              drag="x"
+              dragConstraints = {{ right:0, left: -width }}
+              initial={{ x: 100 }}
+              animate={{ x: 0 }}
+              >
+            {deputados.map(item => (
+              <motion.div className={styles.item} key={item}>
+              <Card className={styles.card}>
+                <Card.Img className={styles.img} variant="top" src={item.urlFoto} />
+                <Card.Body>
+                  <Card.Title>{item.nome}</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Ver mais</Button>
+                </Card.Body>
+              </Card>
+              </motion.div>
+            ))}
+            </motion.div>
+            </motion.div>
           </Col>
           <Col></Col>
         </Row>
